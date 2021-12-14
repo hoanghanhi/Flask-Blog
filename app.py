798b0d13f968bce70e642e5391c376775968a4d9
Hoang Ha Nhi <hoanghanhitg@gmail.com>
2021-12-14 15:46:54 +0700

    hi

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from website.initial import create_app
+from flask import Flask, render_template, request, redirect, url_for
 
 app = create_app()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__== '__main__':
     app.run(debug=True)
